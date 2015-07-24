f3c23f259b8884b0967da0864681538b4682fc3d
Lucian Novosel <luciannovosel@Lucians-MacBook-Pro.local>
2015-07-24 00:17:52 -0700

    Adding pages

---
+++ C-implementation/sandboxes/modal-sandbox/js/handle-bars.js
@@ -0,0 +1,21 @@
+// $( document ).ready(
+//   var data,
+//       source,
+//       template;
+  
+//   // Our data object
+//   data = {
+//     name: "Scott"	
+//   };
+  
+//   // Get the template source
+//   source = $("#my-template").html();
+	  
+//   // Compile the template into a Handlebars function
+//   template = Handlebars.compile(source);
+	
+//   // Pass our data object to the compiled Handlebars function
+//   // Insert back into the page
+// 	$('#welcome-message').html(template(data));
+
+// );
